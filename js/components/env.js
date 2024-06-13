d0ab8ced2c74d1c31763c1cb5d2c3909b733105a
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-06-13 09:15:52 -0500

    feat: :construction: Busqueda por categoria determinada
    Segun las categorias listadas, al usuario ingresar un producto se va a traer dicho producto dependiendo tambien de su categoria asignada

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': 'dd69ded540mshbd5d293ad210adap154616jsna193902f7bab',
-    'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
+    'x-rapidapi-key': '74397e0c0emsh6fb057e4ed07b5dp164777jsn0d7121f63eac',
+	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
