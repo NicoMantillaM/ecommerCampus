32d2c42ff7cc4fd9eedfd64304afddad9007f2ff
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-06-26 23:47:58 -0500

    feat: :sparkles: Producto Random
    Archivos index.html, env.js, gallery.js, main.js y app.js actualizados con cambios en la entrada de búsqueda, la clave API y la funcionalidad de la galería de productos

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': '4350962e28mshd4134133e7bd519p12eb42jsna0277123e05c',
+    'x-rapidapi-key': '15b107dfc2msh5197390cadaa477p1bc842jsndf70c78df1aa',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
